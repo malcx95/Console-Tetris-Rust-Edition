f63204832f8c53eaf26aa217365b6739bd5b4c6c
Malcolm Wigren <trekommafem2@gmail.com>
2019-01-12 16:58:40 +0100

    Add more stuff

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ mod tetrisboard;
 mod squaretypes;
 use crate::tetrisboard::*;
 
+
+fn print_board(board: TetrisBoard) {
+    
+}
+
+
 fn main() {
 
-    //let mut board: TetrisBoard = TetrisBoard::new(10, 10);
+    let mut board: TetrisBoard = TetrisBoard::new(10, 10);
     //println!("{:?}", BODIES);
 }
 
--- src/tetrisboard.rs
+++ src/tetrisboard.rs
@@ -8,19 +8,26 @@ pub struct TetrisBoard {
 
     pub board: Vec<Vec<SquareType>>,
 
-    pub tetromino_pos: (i32, i32),
-    pub curr_tetromino: TetrominoType,
+    pub tetromino_pos: Option<(i32, i32, TetrominoType)>,
 
 }
 
 impl TetrisBoard {
 
-    pub fn new(width: i32, height: i32) {
-        let mut board: Vec<&Vec<SquareType>> = Vec::new();
-        for y in 0..height {
-            for x in 0..width {
-                // TODO not done
+    pub fn new(width: i32, height: i32) -> Self {
+        let mut board: Vec<Vec<SquareType>> = Vec::new();
+        for _ in 0..height {
+            let mut row: Vec<SquareType> = Vec::new();
+            for _ in 0..width {
+                row.push(SquareType::EMPTY);
             }
+            board.push(row);
+        }
+        Self {
+            height: height,
+            width: width,
+            board: board,
+            tetromino_pos: None,
         }
     }
 
